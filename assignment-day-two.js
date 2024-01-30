e77e739099addc96a4d8317d50f5f50abaaf6d86
Pranjal Sinha <158136420+pranjalsinha-coditas@users.noreply.github.com>
2024-01-30 20:06:10 +0530

    Update assignment-day-two.js

--- assignment-day-two.js
+++ assignment-day-two.js
@@ -1,27 +1,37 @@
-// Question-1
-let employee_id;
-let employee_name; 
-let employee_salary; 
-let employee_mail;
+// Declare four variables without assigning values
+// Declare four variables with assigned values
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+// Declare variables to store your father name, mother name, and fatherAge in a single line
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
 
-// Question-2
-let assignedVar1 = 10;
-const gravity = 9.8; 
-const PI = 3.14;
-let isLoggedin = false;
+
+// Const cannot be used because we cannot assign the values. Since it gives error undefined. var we don`t use beacuse of scope related issues
+//Question-1
+let name;
+let my_age;
+let sex;
+let nationality;
+
+//Question-2
+const gravity = 9.8; //gravitational accelaration constant
+const PI = 3.14; //value of pi is always constant
+const boiling_point = 100; //boiling point of water
+const density = 1 //density of water
 
 //Question-3
-const firstName = "Pranjal";
-const lastName = "Sinha";
-const maritalStatus = "Single";
-let country = "India";
-const age = 21;
+//Below I have not used const becuase of overwriting issues
+const first_name = 'Pranjal';
+const last_name = 'Sinha';
+const marital_status = 'Single';
+const country = 'India';
+const age = '21'
 
 //Question-4
-let fatherName, motherName, fatherAge;
+let father_name = 'R.K. Sinha', mother_name = 'P. Sinha', fatherAge = 60;
 
-//Question-5;
-const myAge = 30;
-const YourAge = 28;
-console.log("my age:", MyAge);
-console.log("Your age:", YourAge);
+//Question-5
+const myAge = '20';
+const yourAge = '40';
+
+console.log(myAge);
+console.log(yourAge);
