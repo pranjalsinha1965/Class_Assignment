2b158c73e86092a87ddb8f543e51f0107875039a
Pranjal Sinha <158136420+pranjalsinha-coditas@users.noreply.github.com>
2024-02-06 16:54:43 +0530

    Update class-assignment-day-two.js

--- class-assignment-day-two.js
+++ class-assignment-day-two.js
@@ -53,16 +53,37 @@ function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-// Casing being followed is camel casing for assignments
+//the golden rules of javascript
+// Casing being followed is camel casing for assignments and alos in practice for wiriting codes with strict case for node.js environment
 // i, j and k should not be used use proper variable declaration and definitions
+
 // What are variables?
+/* Ans. Variables are used to store values. Javascript natively supports three basic types namely strings, boolean and numbers.the constant type 
+is not supposed to be changed as the program runs. Literals are variables not only declared like strings, numbers and objects but they are not assigned
+values */
+
 // How can you access the string s present in an array using loops in javascript?
+/* let i=0;
+do {
+    console.log(evens[i]);
+}
+    while(i < evens.length)
+*/
 // tell the difference in between let and var 
+/*let =>block scope and values can be reassigned but it gives referrence error
+var =>has functional scope and values can be reassigned but it gives another kind of error */
+
 // const is always given an initial value and it can`t be reassigned again
+
 // build a function using const to display full name 
+
 // if node isn`t installed then use .html file extension
+
 // always explain the function execution context and global execution context along the with output of the programme
+
 // JIT, JVM, JRE Just in time compiler
+/* go through the memory execution phase of chai aur code tutorial*/
+
 // what javascript engine does actually ?
 // define a function for defining 
 // let and const block scoped
